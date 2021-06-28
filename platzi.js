e9305a98df03adb22b061e164ec4ed427b911984
JMR-Mejia <chucho4720013@gmail.com>
2021-06-28 12:12:28 -0500

    implementing a proxy in MiniVue

--- platzi.js
+++ platzi.js
@@ -2,16 +2,25 @@ class PlatziReactive {
   constructor(options) {
     this.origen = options.data();
 
+    this.$data = new Proxy(this.origen, {
+      get(target, name) {
+        if (name in target) {
+          return target[name];
+        }
+
+        console.warn("La propiedad", name, "no existe");
+      },
+    });
   }
 
   mount() {
     document.querySelectorAll("*[p-text]").forEach((el) => {
-      this.pText(el, this.origen, el.getAttribute("p-text"));
+      this.pText(el, this.$data, el.getAttribute("p-text"));
     });
   }
 
   pText(el, target, name) {
-    el.innerText = target[name]
+    el.innerText = target[name];
   }
 
   pModel() {}
